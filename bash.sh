a7a2e9c1f7dfb7f4be855fea860f4dfea97521f1
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-31 02:18:23 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,3 @@
+docker run -it --rm mcr.microsoft.com/azurelinux/base/core:4.0
+docker run -it  mcr.microsoft.com/azurelinux-beta/base/core:4.0
+docker run -it --rm mcr.microsoft.com/azurelinux/base/core
